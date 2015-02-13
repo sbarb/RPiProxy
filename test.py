535dc961e58b361ffb5b3e60ed7a8409221ad1da
steven <steven.barber@eagles.usm.edu>
2015-02-13 05:59:50 -0600

    starting up

---
+++ test.py
@@ -0,0 +1,43 @@
+import RPi.GPIO as GPIO
+import time
+
+isOn = False
+output = 7
+doneStr = ""
+
+#setup gpio pinout using BOARD numbering
+GPIO.setmode(GPIO.BOARD)
+#ignore warnings
+GPIO.setwarnings(False)
+#setup pin for output
+GPIO.setup(output, GPIO.OUT)
+
+def cutOnLED():
+	GPIO.output(output, True)
+	print "Going ON"
+	return True
+
+def cutOffLED():
+	GPIO.output(output, False)
+	print "Going OFF"
+	return False
+def cleanUp():
+	cutOffLED()
+	GPIO.cleanup() #cleanup all gpio
+	
+try:
+	while (doneStr != 'stop'):
+		doneStr = str(raw_input("Turn light on?\t"))
+		if doneStr in ['Y', 'y', 'YES', 'Yes', 'yes']:
+			isOn = cutOnLED()
+       
+		doneStr = str(raw_input("Enter stop to exit...\nTurn light off?	"))
+		if doneStr in ['Y', 'y', 'YES', 'Yes', 'yes']:
+			isOn = cutOffLED()
+	
+except KeyboardInterrupt:
+	cleanUp()
+
+print "\n\n\nDone"
+
+GPIO.cleanup()
