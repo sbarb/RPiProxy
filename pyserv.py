4a6689577b7a54de5000587c256c95bc1043d326
steven <steven.barber@eagles.usm.edu>
2015-02-13 22:10:59 -0600

    revisions

--- pyserv.py
+++ pyserv.py
@@ -6,7 +6,7 @@ import time
 
 isOn = "OFF"
 notIsOn = "ON"
-output = 7
+outputPin = 7
 doneStr = ""
 
 #setup gpio pinout using BOARD numbering
@@ -14,33 +14,20 @@ GPIO.setmode(GPIO.BOARD)
 #ignore warnings
 GPIO.setwarnings(False)
 #setup pin for output
-GPIO.setup(output, GPIO.OUT)
+GPIO.setup(outputPin, GPIO.OUT)
 
 def cutOnLED():
-	GPIO.output(output, True)
+	GPIO.output(outputPin, True)
 	print "Going ON"
 	return True
 
 def cutOffLED():
-	GPIO.output(output, False)
+	GPIO.output(outputPin, False)
 	print "Going OFF"
 	return False
 def cleanUp():
 	cutOffLED()
 	GPIO.cleanup() #cleanup all gpio
-	
-#try:
-#	while (doneStr != 'stop'):
-#		doneStr = str(raw_input("Turn light on?\t"))
-#		if doneStr in ['Y', 'y', 'YES', 'Yes', 'yes']:
-#			isOn = cutOnLED()
-#      
-#		doneStr = str(raw_input("Enter stop to exit...\nTurn light off?	"))
-#		if doneStr in ['Y', 'y', 'YES', 'Yes', 'yes']:
-#			isOn = cutOffLED()
-#	
-#except KeyboardInterrupt:
-#	cleanUp()
 
 app = Flask(__name__)
 
@@ -56,23 +43,24 @@ def LEDinfo():
 	elif isOn == "OFF":
 		notIsOn = "ON"
 		
-	print "LED = ", isOn
-		
 	if request.form['LED'] == "ON":
 		isOn = "ON"
 		notIsOn = "OFF"
 		cutOnLED()
-	elif request.form['LED'] == "OOFF":
+	elif request.form['LED'] == "OFF":
 		isOn = "OFF"
 		notIsOn = "ON"
 		cutOffLED()
-
+		
+	print "LED = ", isOn
 	return render_template('index.html', isOn=isOn, notIsOn=notIsOn)
 	
 if __name__ == "__main__":
 	app.debug=True
 	app.run("0.0.0.0")
+
+
 	
 print "\n\n\nDone"
 
-# GPIO.cleanup()
+GPIO.cleanup()
