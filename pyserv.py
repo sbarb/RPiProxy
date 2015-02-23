feda97b0b7862b0e2741507105f1f2398d0497f2
Steven Barber <steven.barber@eagles.usm.edu>
2015-02-22 19:37:52 -0600

    **ADD LED2, LED3***

--- pyserv.py
+++ pyserv.py
@@ -5,8 +5,12 @@ import time
 
 isOnLamp = False
 isOnLed = False
+isOn2Led = False
+isOn3Led = False
 light = 7
 led = 11
+led2 = 13
+led3 = 15
 
 #setup gpio pinout using BOARD numbering
 GPIO.setmode(GPIO.BOARD)
@@ -15,6 +19,8 @@ GPIO.setwarnings(False)
 #setup pin for output
 GPIO.setup(light, GPIO.OUT)
 GPIO.setup(led, GPIO.OUT)
+GPIO.setup(led2, GPIO.OUT)
+GPIO.setup(led3, GPIO.OUT)
 
 def writeHigh(pin):
   GPIO.output(pin, True)
@@ -27,8 +33,12 @@ def writeLow(pin):
 def cleanUp():
   if isOnLamp:
     writeLow(light) # cut light off
-  if isOnLed:
+  elif isOnLed:
     writeLow(led) # cut led off
+  elif isOn2Led:
+    writeLow(led2) # cut led2 off
+  elif isOn3Led:
+    writeLow(led3) # cut led3 off
   GPIO.cleanup() # cleanup all gpio 
 
 app = Flask(__name__)
@@ -37,27 +47,41 @@ app = Flask(__name__)
 def index():
   print "/ Light isOn = " + str(isOnLamp)
   print "/ LED isOn = " + str(isOnLed)
-  return render_template('index.html', isOnLamp=isOnLamp, isOnLed=isOnLed)
+  return render_template('index.html', isOnLamp=isOnLamp, isOnLed=isOnLed, isOnLed=isOn2Led, isOnLed=isOn3Led)
 
 @app.route("/LEDinfo", methods=['POST', 'GET'])
 def LEDinfo():
-  global isOnLamp, isOnLed
+  global isOnLamp, isOnLed, isOnLed2, isOnLed3
   lampIsOn = request.form.get('Lamp')
   ledIsOn = request.form.get('LED')
+  led2IsOn = request.form.get('LED2')
+  led3IsOn = request.form.get('LED3')
   # print "Going " + ledIsOn
 
   if lampIsOn == "ON": 
     isOnLamp = writeHigh(light)
   elif lampIsOn == "OFF": 
     isOnLamp = writeLow(light)
+
   elif ledIsOn == "ON":
     isOnLed = writeHigh(led)
   elif ledIsOn == "OFF": 
     isOnLed = writeLow(led)
+
+  elif led2IsOn == "ON":
+    isOnLed2 = writeHigh(led2)
+  elif led2IsOn == "OFF": 
+    isOnLed2 = writeLow(led2)
+
+  elif led3IsOn == "ON":
+    isOnLed3 = writeHigh(led3)
+  elif led3IsOn == "OFF": 
+    isOnLed3 = writeLow(led3)
+
   else:
     print "Got an unexepected request."
   
-  print ledIsOn
+  # print ledIsOn
   return redirect('/')
 
 if __name__ == "__main__": 
