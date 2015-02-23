c43df8f080b874dee2a109e8ca751d2943b0da19
Jake Gaylor <jhgaylor@gmail.com>
2015-02-23 02:39:39 -0600

    updates

--- test.py
+++ test.py
@@ -1,104 +1,83 @@
-from flask import Flask, request, render_template, redirect
-from jinja2 import Template
-# import RPi.GPIO as GPIO
-import time
+import requests as R
+from time import sleep
+from random import choice
 
-pin1 = 7
-pin2 = 11
-pin3 = 13
-pin4 = 15
-isOn1 = False
-isOn2 = False
-isOn3 = False
-isOn4 = False
 
-# #setup gpio pinout using BOARD numbering
-# GPIO.setmode(GPIO.BOARD)
-# #ignore warnings
-# GPIO.setwarnings(False)
-# #setup pin for output
-# GPIO.setup(pin1, GPIO.OUT)
-# GPIO.setup(pin2, GPIO.OUT)
-# GPIO.setup(pin3, GPIO.OUT)
-# GPIO.setup(pin4, GPIO.OUT)
+import wave, struct
 
-# def writeHigh(pin):
-#   GPIO.output(pin, True)
-#   return True
+waveFile = wave.open('sine.wav', 'r')
 
-# def writeLow(pin):
-#   GPIO.output(pin, False)
-#   return False
+length = waveFile.getnframes()
+for i in range(0,length):
+    waveData = waveFile.readframes(1)
+    data = struct.unpack("<h", waveData)
+    print int(data[0])
+
+URL = 'http://108.178.248.104/LEDinfo'
+# state should be 0 or 1
+def l(state=0):
+  state = str(state)
+  return {"Lamp": state}
+def a(state=0):
+  state = str(state)
+  return {"Alpha": state}
+def t(state=0):
+  state = str(state)
+  return {"pin3": state}
+def f(state=0):
+  state = str(state)
+  return {"pin4": state}
+
   
-# def cleanUp():
-#   if isOn1:
-#     writeLow(pin1) # cut pin1 off
-#   elif isOn2:
-#     writeLow(pin2) # cut pin2 off
-#   elif isOn3:
-#     writeLow(pin3) # cut pin3 off
-#   elif isOn4:
-#     writeLow(pin4) # cut pin4 off
-#   GPIO.cleanup() # cleanup all gpio 
+def _(*args):
+  data = dict(obj.items()[0] for obj in args)
+  R.post(URL, data=data)
 
-app = Flask(__name__)
 
-@app.route("/", methods=['GET'])
-def index():
-  print "/ Lamp = pin1 = " + str(isOn1)
-  print "/ LED 1 = pin2 = " + str(isOn2)
-  print "/ LED 2 = pin3 = " + str(isOn3)
-  print "/ LED 3 = pin4 = " + str(isOn4)
-  return render_template('index.html', isOn1=isOn1, isOn2=isOn2, isOn3=isOn3, isOn4=isOn4)
+_(l(1), a(0), t(1), f(0))
 
-@app.route("/LEDinfo", methods=['POST', 'GET'])
-def LEDinfo():
-  global isOn1, isOn2, isOn3, isOn4
-  pin1IsOn = request.form.get('lamp')
-  pin2IsOn = request.form.get('led1')
-  pin3IsOn = request.form.get('led2')
-  pin4IsOn = request.form.get('led3')
-  
-  if pin1IsOn == "1": 
-  	print "light on"
-  	isOn1 = True
-  elif pin1IsOn == "0":
-  	print "light off" 
-  	isOn1 = False
+def pulsate(times):
+  if times < 1:
+    return True
+  _(a(1))
+  sleep(.01)
+  _(t(1))
+  sleep(.01)
+  _(a(0), f(1))
+  sleep(.01)
+  _(t(0))
+  sleep(.01)
+  _(f(0))
+  pulsate(times-1)
 
-  elif pin2IsOn == "1":
-  	print "led 1 on"
-  	isOn2 = True
-  elif pin2IsOn == "0": 
-  	print "led 1 off"
-  	isOn2 = False
+def rando(times):
+  if times < 1:
+    return True
+  data = []
+  # do one random choice per light
+  choiceA = choice([1, 0])
+  data.append(a(choiceA))
+  choiceT = choice([1, 0])
+  data.append(t(choiceA))
+  choiceF = choice([1, 0])
+  print choiceA, choiceT, choiceF
+  data.append(f(choiceA))
+  _(*data)
+  sleep(.5)
+  rando(times-1)
 
-  elif pin3IsOn == "1":
-  	print "led 2 on"
-  	isOn3 = True
-  elif pin3IsOn == "0":
-  	print "led 2 off" 
-  	isOn3 = False
+def flicker(times):
+  if times < 1:
+    return True
+  _(a(1), t(1), f(1))  
+  _(a(0), t(0), f(0))
+  sleep(.3)
+  flicker(times-1)
+# flicker(5)
+# rando(20)
+# # pulsate(30)
+# flicker(5)
 
-  elif pin4IsOn == "1":
-    print "led 3 on"
-    isOn4 = True
-  elif pin4IsOn == "0": 
-  	print "led 3 off"
-  	isOn4 = False
-
-  else:
-    print "Got an unexepected request."
-  
-  return redirect('/')
-
-if __name__ == "__main__": 
-  try:
-    app.debug = True
-    app.run("0.0.0.0")
-    raise KeyboardInterrupt
-
-  finally:
-    print "\n\n\nServer Run Complete."
-    # cleanUp()
-    print "GPIO Cleanup Complete"
+# lampOn()
+# sleep(1)
+# lampOff()
