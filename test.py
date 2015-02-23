09d0db747dc40a3218ed81a3378e2b43edbd4609
Jake Gaylor <jhgaylor@gmail.com>
2015-02-23 03:32:43 -0600

    make it dance

--- test.py
+++ test.py
@@ -6,37 +6,73 @@ URL = 'http://108.178.248.104/LEDinfo'
 # state should be 0 or 1
 def l(state=0):
   state = str(state)
-  return {"Lamp": state}
-def a(state=0):
+  return {"pin1": state}
+def q(state=0):
   state = str(state)
-  return {"Alpha": state}
-def t(state=0):
+  return {"pin2": state}
+def w(state=0):
   state = str(state)
   return {"pin3": state}
-def f(state=0):
+def e(state=0):
   state = str(state)
   return {"pin4": state}
+def r(state=0):
+  state = str(state)
+  return {"pin5": state}
+def t(state=0):
+  state = str(state)
+  return {"pin6": state}
+def y(state=0):
+  state = str(state)
+  return {"pin7": state}
+def u(state=0):
+  state = str(state)
+  return {"pin8": state}
+def i(state=0):
+  state = str(state)
+  return {"pin9": state}
+def o(state=0):
+  state = str(state)
+  return {"pin10": state}
+def p(state=0):
+  state = str(state)
+  return {"pin11": state}
 
   
 def _(*args):
   data = dict(obj.items()[0] for obj in args)
   R.post(URL, data=data)
+  return sleep
 
+def all(state):
+  return [q(state), w(state), e(state), r(state), t(state), y(state), u(state), i(state), o(state), p(state)]
 
-_(l(1), a(0), t(1), f(0))
+def each():
+  return [q,w,e,r,t,y,u,i,o,p]
 
 def pulsate(times):
   if times < 1:
     return True
-  _(a(1))
-  sleep(.01)
-  _(t(1))
-  sleep(.01)
-  _(a(0), f(1))
-  sleep(.01)
-  _(t(0))
-  sleep(.01)
-  _(f(0))
+  _(q(1))(.01)
+  _(w(1))(.01)
+  _(e(1))(.01)
+  _(r(1))(.01)
+  _(t(1))(.01)
+  _(y(1))(.01)
+  _(u(1))(.01)
+  _(i(1))(.01)
+  _(o(1))(.01)
+  _(p(1))(.01)
+  _(q())(.01)
+  _(w())(.01)
+  _(e())(.01)
+  _(r())(.01)
+  _(t())(.01)
+  _(y())(.01)
+  _(u())(.01)
+  _(i())(.01)
+  _(o())(.01)
+  _(p())
   pulsate(times-1)
 
 def rando(times):
@@ -44,28 +80,23 @@ def rando(times):
     return True
   data = []
   # do one random choice per light
-  choiceA = choice([1, 0])
-  data.append(a(choiceA))
-  choiceT = choice([1, 0])
-  data.append(t(choiceT))
-  choiceF = choice([1, 0])
-  print choiceA, choiceT, choiceF
-  data.append(f(choiceF))
-  _(*data)
-  sleep(.5)
+  for thing in each():
+    c = choice([1, 0])
+    data.append(thing(c))
+  _(*data)(0.5)
   rando(times-1)
 
 def flicker(times):
   if times < 1:
     return True
-  _(a(1), t(1), f(1))  
-  _(a(0), t(0), f(0))
-  sleep(.3)
+  _(*all(1))(0.1)  
+  _(*all(0))(0.1)
   flicker(times-1)
-flicker(5)
-# rando(20)
-pulsate(30)
-flicker(5)
+
+flicker(3)
+rando(800)
+# pulsate(10)
+flicker(3)
 
 # lampOn()
 # sleep(1)
