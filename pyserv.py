3ba6ca3acad2672e00009a50aee708e27032b364
Steven Barber <steven.barber@eagles.usm.edu>
2015-02-24 00:38:45 -0600

    adding pin['name']

--- pyserv.py
+++ pyserv.py
@@ -30,17 +30,17 @@ if isPi:
 # mapping of physical pin (and human readable name) to the internal GPIO pin numbering
 # ie: {name: data}
 _unordered_pins = {
-  'Lamp': { 'pin': 7, 'state': False, 'weight': 1 },
-  'LED 1': { 'pin': 11, 'state': False, 'weight': 2 },
-  'LED 2': { 'pin': 13, 'state': False, 'weight': 3 },
-  'LED 3': { 'pin': 15, 'state': False, 'weight': 4 },
-  'LED 4': { 'pin': 12, 'state': False, 'weight': 5 },
-  'LED 5': { 'pin': 16, 'state': False, 'weight': 6 },
-  'LED 6': { 'pin': 18, 'state': False, 'weight': 7 },
-  'LED 7': { 'pin': 22, 'state': False, 'weight': 8 },
-  'LED 8': { 'pin': 24, 'state': False, 'weight': 9 },
-  'LED 9': { 'pin': 26, 'state': False, 'weight': 10 },
-  'LED 10': { 'pin': 19, 'state': False, 'weight': 11 }
+  'pin1': { 'pin': 7, 'name' : Lamp, 'state': False, 'weight': 1 },
+  'pin2': { 'pin': 11, 'name' : LED 1, 'state': False, 'weight': 2 },
+  'pin3': { 'pin': 13, 'name' : LED 2, 'state': False, 'weight': 3 },
+  'pin4': { 'pin': 15, 'name' : LED 3, 'state': False, 'weight': 4 },
+  'pin5': { 'pin': 12, 'name' : LED 4, 'state': False, 'weight': 5 },
+  'pin6': { 'pin': 16, 'name' : LED 5, 'state': False, 'weight': 6 },
+  'pin7': { 'pin': 18, 'name' : LED 6, 'state': False, 'weight': 7 },
+  'pin8': { 'pin': 22, 'name' : LED 7, 'state': False, 'weight': 8 },
+  'pin9': { 'pin': 24, 'name' : LED 8, 'state': False, 'weight': 9 },
+  'pin10': { 'pin': 26, 'name' : LED 9, 'state': False, 'weight': 10 },
+  'pin11': { 'pin': 19, 'name' : LED 10, 'state': False, 'weight': 11 }
 }
 # guarantees order during iteration based on the weight key
 pins = OrderedDict(sorted(_unordered_pins.items(), key=lambda t: t[1]['weight']))
