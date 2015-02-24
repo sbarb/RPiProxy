167e838cec8b13367089d51d42c2f54428cc0ac0
Steven Barber <steven.barber@eagles.usm.edu>
2015-02-24 02:55:56 -0600

    working on sockets

--- socketServer.py
+++ socketServer.py
@@ -1,115 +1,24 @@
-import logging
-import sys
 import SocketServer
 
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(name)s: %(message)s',
-                    )
-
-class EchoRequestHandler(SocketServer.BaseRequestHandler):
-    
-    def __init__(self, request, client_address, server):
-        self.logger = logging.getLogger('EchoRequestHandler')
-        self.logger.debug('__init__')
-        SocketServer.BaseRequestHandler.__init__(self, request, client_address, server)
-        return
-
-    def setup(self):
-        self.logger.debug('setup')
-        return SocketServer.BaseRequestHandler.setup(self)
+class MyTCPHandler(SocketServer.StreamRequestHandler):
 
     def handle(self):
-        self.logger.debug('handle')
+        # self.rfile is a file-like object created by the handler;
+        # we can now use e.g. readline() instead of raw recv() calls
+        self.data = self.rfile.readline().strip()
+        print "{} wrote:".format(self.client_address[0])
+        print self.data
+        # Likewise, self.wfile is a file-like object used to write back
+        # to the client
+        self.wfile.write(self.data.upper())
 
-        # Echo the back to the client
-        data = self.request.recv(1024)
-        self.logger.debug('recv()->"%s"', data)
-        self.request.send(data)
-        return
 
-    def finish(self):
-        self.logger.debug('finish')
-        return SocketServer.BaseRequestHandler.finish(self)
+if __name__ == "__main__":
+    HOST, PORT = "192.168.1.111", 9999
 
-class EchoServer(SocketServer.TCPServer):
-    
-    def __init__(self, server_address, handler_class=EchoRequestHandler):
-        self.logger = logging.getLogger('EchoServer')
-        self.logger.debug('__init__')
-        SocketServer.TCPServer.__init__(self, server_address, handler_class)
-        return
+    # Create the server, binding to localhost on port 9999
+    server = SocketServer.TCPServer((HOST, PORT), MyTCPHandler)
 
-    def server_activate(self):
-        self.logger.debug('server_activate')
-        SocketServer.TCPServer.server_activate(self)
-        return
-
-    def serve_forever(self):
-        self.logger.debug('waiting for request')
-        self.logger.info('Handling requests, press <Ctrl-C> to quit')
-        while True:
-            self.handle_request()
-        return
-
-    def handle_request(self):
-        self.logger.debug('handle_request')
-        return SocketServer.TCPServer.handle_request(self)
-
-    def verify_request(self, request, client_address):
-        self.logger.debug('verify_request(%s, %s)', request, client_address)
-        return SocketServer.TCPServer.verify_request(self, request, client_address)
-
-    def process_request(self, request, client_address):
-        self.logger.debug('process_request(%s, %s)', request, client_address)
-        return SocketServer.TCPServer.process_request(self, request, client_address)
-
-    def server_close(self):
-        self.logger.debug('server_close')
-        return SocketServer.TCPServer.server_close(self)
-
-    def finish_request(self, request, client_address):
-        self.logger.debug('finish_request(%s, %s)', request, client_address)
-        return SocketServer.TCPServer.finish_request(self, request, client_address)
-
-    def close_request(self, request_address):
-        self.logger.debug('close_request(%s)', request_address)
-        return SocketServer.TCPServer.close_request(self, request_address)
-
-if __name__ == '__main__':
-    import socket
-    import threading
-
-    address = ('192.168.1.111', 8887) # let the kernel give us a port
-    server = EchoServer(address, EchoRequestHandler)
-    ip, port = server.server_address # find out what port we were given
-    # ip = '192.168.1.111'
-    # port = '8888'
-
-    t = threading.Thread(target=server.serve_forever)
-    t.setDaemon(True) # don't hang on exit
-    t.start()
-
-    logger = logging.getLogger('client')
-    logger.info('Server on %s:%s', ip, port)
-
-    # Connect to the server
-    logger.debug('creating socket')
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    logger.debug('connecting to server')
-    s.connect((ip, port))
-
-    # Send the data
-    message = 'Hello, world'
-    logger.debug('sending data: "%s"', message)
-    len_sent = s.send(message)
-
-    # Receive a response
-    logger.debug('waiting for response')
-    response = s.recv(len_sent)
-    logger.debug('response from server: "%s"', response)
-
-    # Clean up
-    logger.debug('closing socket')
-    s.close()
-    logger.debug('done')
-    server.socket.close()
+    # Activate the server; this will keep running until you
+    # interrupt the program with Ctrl-C
+    server.serve_forever()
