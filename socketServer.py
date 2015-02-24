c1706342f9e75586fbdf9b37b1c62e0af402a86c
Steven Barber <steven.barber@eagles.usm.edu>
2015-02-24 03:34:12 -0600

    merging samples

--- socketServer.py
+++ socketServer.py
@@ -1,6 +1,23 @@
+import logging
+import sys
 import SocketServer
 
-class MyTCPHandler(SocketServer.StreamRequestHandler):
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(name)s: %(message)s',
+                    )
+
+
+class TCPHandler(SocketServer.StreamRequestHandler):
+
+    def __init__(self, request, client_address, server):
+        self.logger = logging.getLogger('EchoRequestHandler')
+        self.logger.debug('__init__')
+        SocketServer.StreamRequestHandler.__init__(self, request, client_address, server)
+        return
+
+    def setup(self):
+        self.logger.debug('setup')
+        return SocketServer.StreamRequestHandler.setup(self)
 
     def handle(self):
         # self.rfile is a file-like object created by the handler;
@@ -12,13 +29,64 @@ class MyTCPHandler(SocketServer.StreamRequestHandler):
         # to the client
         self.wfile.write(self.data.upper())
 
+    def finish(self):
+        self.logger.debug('finish')
+        return SocketServer.StreamRequestHandler.finish(self)
+
+class myServer(SocketServer.TCPServer):
+    
+    def __init__(self, server_address, handler_class=EchoRequestHandler):
+        self.logger = logging.getLogger('EchoServer')
+        self.logger.debug('__init__')
+        SocketServer.TCPServer.__init__(self, server_address, handler_class)
+        return
+
+    def server_activate(self):
+        self.logger.debug('server_activate')
+        SocketServer.TCPServer.server_activate(self)
+        return
+
+    def serve_forever(self):
+        self.logger.debug('waiting for request')
+        self.logger.info('Handling requests, press <Ctrl-C> to quit')
+        while True:
+            self.handle_request()
+        return
+
+    def handle_request(self):
+        self.logger.debug('handle_request')
+        return SocketServer.TCPServer.handle_request(self)
+
+    def verify_request(self, request, client_address):
+        self.logger.debug('verify_request(%s, %s)', request, client_address)
+        return SocketServer.TCPServer.verify_request(self, request, client_address)
+
+    def process_request(self, request, client_address):
+        self.logger.debug('process_request(%s, %s)', request, client_address)
+        return SocketServer.TCPServer.process_request(self, request, client_address)
+
+    def server_close(self):
+        self.logger.debug('server_close')
+        return SocketServer.TCPServer.server_close(self)
+
+    def finish_request(self, request, client_address):
+        self.logger.debug('finish_request(%s, %s)', request, client_address)
+        return SocketServer.TCPServer.finish_request(self, request, client_address)
+
+    def close_request(self, request_address):
+        self.logger.debug('close_request(%s)', request_address)
+        return SocketServer.TCPServer.close_request(self, request_address)
 
 if __name__ == "__main__":
     HOST, PORT = "192.168.1.111", 9999
+    address = (HOST, PORT)
 
     # Create the server, binding to localhost on port 9999
-    server = SocketServer.TCPServer((HOST, PORT), MyTCPHandler)
+    server = myServer((HOST, PORT), TCPHandler)
 
     # Activate the server; this will keep running until you
     # interrupt the program with Ctrl-C
     server.serve_forever()
+
+    logger = logging.getLogger('client')
+    logger.info('Server on %s:%s', ip, port)
